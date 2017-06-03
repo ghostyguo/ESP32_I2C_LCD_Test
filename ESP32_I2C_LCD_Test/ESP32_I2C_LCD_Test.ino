990bde44a5678ac0128806e1d4ffa44b0307ffe9
ghostyguo <ghostyguo@gmail.com>
2017-06-04 00:37:35 +0800

    Add files via upload

---
+++ ESP32_I2C_LCD_Test/ESP32_I2C_LCD_Test.ino
@@ -0,0 +1,24 @@
+#include <Wire.h>
+#include "LiquidCrystal_I2C.h"
+
+//LiquidCrystal_I2C lcd(0x27);  // Set the LCD I2C address
+LiquidCrystal_I2C  lcd(0x27,2,1,0,4,5,6,7); // 0x27 is the I2C bus address for an unmodified backpack
+
+void setup()
+{
+  Serial.begin(115200);
+
+  // activate LCD module
+  lcd.begin (16,2); // for 16 x 2 LCD module
+  lcd.setBacklightPin(3,POSITIVE);
+  lcd.setBacklight(HIGH);
+}
+
+void loop()
+{  
+  lcd.home ();                   // go home
+  lcd.print("Hello, ESP32");  
+  lcd.setCursor ( 0, 1 );        // go to the next line
+  lcd.print (" FORUM - fm   ");
+  delay ( 1000 );
+}
